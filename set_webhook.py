3ed9a29df7857b3e5cacb658c836b81df8b9bf1b
Esgrima88 <esthermarinho1107@gmail.com>
2025-04-05 00:06:33 -0300

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,18 +1,1 @@
-import os
-import requests
 
-# Certifique-se de que a variável BOT_TOKEN está definida no Render
-TOKEN = os.getenv("BOT_TOKEN")
-
-# URL do seu app no Render (ajuste se mudar futuramente)
-WEBHOOK_URL = f"https://charlicore-telegram-1.onrender.com/{TOKEN}"
-
-# Endpoint da API do Telegram para configurar o webhook
-TELEGRAM_API_URL = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}"
-
-# Faz a requisição
-response = requests.get(TELEGRAM_API_URL)
-
-# Exibe o resultado
-print("Status:", response.status_code)
-print("Resposta:", response.json())
