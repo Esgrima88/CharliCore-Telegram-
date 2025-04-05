4214a2910e0e3c6e28b30952f2f8d457ad789013
Esgrima88 <esthermarinho1107@gmail.com>
2025-04-04 23:13:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ app = ApplicationBuilder().token("7741377039:AAHHERZOFt8-JSCF28UUXgEInpH06CldfZQ
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
+        await bot.set_webhook(url=f"{RENDER_URL}/{TOKEN}")
+
+    asyncio.run(run())
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
